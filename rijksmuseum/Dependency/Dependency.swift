89fe8cec6efb35db868af18f740c4f1add37813f
dmitryshliugaev <shegodmal@gmail.com>
2022-08-10 12:10:02 +0300

    Fix Dependency singleton

--- rijksmuseum/Dependency/Dependency.swift
+++ rijksmuseum/Dependency/Dependency.swift
@@ -10,6 +10,8 @@ import Foundation
 class Dependency {
     static let sharedInstance = Dependency()
     
+    private init() {}
+    
     lazy var networkEnvironment: NetworkEnvironmentProtocol = {
         return RijksEnvironment()
     }()
